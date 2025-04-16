54de4a97baadef96ab02e5dc5b470c262705e29d
Giovany Dominguez <rastherdev@gmail.com>
2025-04-16 12:30:08 -0600

    variables

---
+++ react-basics/src/variables.js
@@ -0,0 +1,15 @@
+// const and let
+const myname = 'Giovany';
+const myLastName = 'Dominguez';
+
+let valor = 5;
+valor = 4;
+
+console.log(myname, myLastName, valor);
+console.log(myname); 
+
+if(true){
+    let valor = 6;
+    console.log(valor);
+}
+console.log(valor);
