f9a0880618d0f9b5ca0234450085b6d1bf01b80f
Giovany Dominguez <rastherdev@gmail.com>
2025-04-21 13:59:03 -0600

    objects updated

--- react-basics/src/objects.js
+++ react-basics/src/objects.js
@@ -2,8 +2,20 @@ const ppl = {
     name: 'Giovany',
     lastName: 'Dominguez',
     age: 28,
+    address: {
+        street: 'El Romeral',
+        no: 3,
+        lat: 102.39192,
+        long: 199.23123,
+        zip:45133,
+    }
 };
 
+const ppl2 = {...ppl};
 console.log(ppl);
+console.log("===========");
+console.log(ppl2);
+
+
 console.log(ppl.name);
 
