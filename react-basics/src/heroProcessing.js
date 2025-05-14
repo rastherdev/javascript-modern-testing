8592377b42034a17fdc4e73810698503d8415f0e
Giovany Dominguez <rastherdev@gmail.com>
2025-05-14 16:02:03 -0600

    multiple exports and imports

--- react-basics/src/heroProcessing.js
+++ react-basics/src/heroProcessing.js
@@ -1,4 +1,4 @@
-import { heroes} from './data.js';
+import  heroes, {owners} from './data.js';
 console.log(heroes);
 
 const getHeroById = (id) => {
@@ -8,7 +8,8 @@ const getHeroById = (id) => {
 const getHeroesByOwner = (owner) => {
     return heroes.filter(hero => hero.owner === owner);
 }
-
+logger.log('====================================');
+console.log(owners);
 console.log('====================================');
 console.log(getHeroById(2));
 console.log(getHeroesByOwner('Marvel'));
